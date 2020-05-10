697ae18b1b9bb0cb8d60cb05751a6bc7bf6a2fd7
AdityaPratamaS3 <63409836+AdityaPratamaS3@users.noreply.github.com>
2020-05-10 11:28:58 +0700

    Add files via upload

---
+++ pertemuan2/latihan1.php
@@ -0,0 +1,45 @@
+<?php 
+//contoh operasi sederhana
+echo 1+1+2;
+echo '<hr>';
+
+//variabel
+//tempat penyimpan nilai
+$nama ='pw';
+echo '<hr>';
+//operator
+//operator assignment (penugasan)
+// = += *= -= /= .=
+$a = 1;
+$b = 2;
+$a = $b;
+echo $a;
+echo '<hr>';
+
+//operator aritmatika
+//- * / + %
+//kukabataku (kurung kali bagi tambah kurang)
+echo 10 $ 4;
+echo'<hr>';
+//operator concat (penggabung string)
+// .
+$nama_depan ='aku';
+$nama_belakang='kamu';
+echo'<hr>';
+
+$nama ='aku';
+$nama ='';
+$nama ='kamu';
+echo $nama;
+echo '<hr>';
+
+//standar output
+// ''  ""
+echo "assalamu'alaikum";
+echo '<hr>';
+echo'A :"hello world"';
+echo '<hr>';
+echo 'A :"hello\'world"';
+
+
+?>
